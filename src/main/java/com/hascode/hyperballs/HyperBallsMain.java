5fea7471930a94aa3ad2a90588382cef0355405a
Micha Kops <micha.kops@gmail.com>
2013-06-09 18:06:31 +0200

    Paddle implementation, game restart and game over notification added.

--- src/main/java/com/hascode/hyperballs/HyperBallsMain.java
+++ src/main/java/com/hascode/hyperballs/HyperBallsMain.java
@@ -16,9 +16,10 @@ import javafx.scene.Scene;
 import javafx.scene.SceneBuilder;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBuilder;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.HyperlinkBuilder;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.LinearGradientBuilder;
-import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CircleBuilder;
 import javafx.scene.shape.Rectangle;
@@ -30,47 +31,75 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class HyperBallsMain extends Application {
+	private final DoubleProperty ballX = new SimpleDoubleProperty();
+	private final DoubleProperty ballY = new SimpleDoubleProperty();
+	private final DoubleProperty paddleX = new SimpleDoubleProperty();
+	private final BooleanProperty gameStopped = new SimpleBooleanProperty();
+	private final BooleanProperty gameLost = new SimpleBooleanProperty(false);
+	private boolean movingDown = true;
+	private boolean movingRight = true;
+	private double movingSpeed = 1.0;
+	private double paddleDragX = 200.0;
+	private double paddleTranslateX = 200.0;
+
 	private final Circle ball = CircleBuilder.create().radius(10.0)
-			.fill(Color.AZURE).build();
+			.fill(Color.BLACK).build();
 
 	private final Rectangle borderTop = RectangleBuilder.create().x(0).y(0)
-			.width(500).height(4).build();
+			.width(500).height(2).build();
 
 	private final Rectangle borderBottom = RectangleBuilder.create().x(0)
-			.y(496).width(500).height(4).build();
+			.y(492).width(500).height(8).build();
 
 	private final Rectangle borderLeft = RectangleBuilder.create().x(0).y(0)
-			.width(4).height(500).build();
+			.width(2).height(500).build();
 
-	private final Rectangle borderRight = RectangleBuilder.create().x(496).y(0)
-			.width(4).height(500).build();
+	private final Rectangle borderRight = RectangleBuilder.create().x(498).y(0)
+			.width(2).height(500).build();
+
+	private final Rectangle paddle = RectangleBuilder.create().x(200).y(460)
+			.width(150).height(15).fill(Color.BLACK)
+			.onMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(final MouseEvent evt) {
+					paddleTranslateX = paddle.getTranslateX();
+					paddleDragX = evt.getSceneX();
+				}
+			}).onMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(final MouseEvent evt) {
+					double y = evt.getSceneX() - paddleDragX;
+					paddleX.setValue(paddleTranslateX + y);
+				}
+			}).build();
 
 	private final Text infotext = TextBuilder.create()
 			.text("Press 'Start' to play!").font(Font.font("Arial", 16.0))
 			.layoutX(540).layoutY(50).build();
+
+	private final Text gameOverText = TextBuilder.create().text("Game Over")
+			.font(Font.font("Arial", 40.0)).fill(Color.RED).layoutX(150)
+			.layoutY(250).build();
 
 	private final Button startButton = ButtonBuilder.create().text("Start")
 			.layoutX(540).layoutY(100)
 			.onAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(final ActionEvent evt) {
+					initGame();
 					gameStopped.set(false);
 					heartbeat.playFromStart();
 				}
 			}).build();
 
+	private final Hyperlink link = HyperlinkBuilder.create()
+			.text("www.hascode.com").layoutX(540).layoutY(480).build();
+
 	private final Group area = GroupBuilder
 			.create()
 			.focusTraversable(true)
 			.children(ball, borderTop, borderBottom, borderLeft, borderRight,
-					startButton, infotext).build();
-
-	private final DoubleProperty ballX = new SimpleDoubleProperty();
-	private final DoubleProperty ballY = new SimpleDoubleProperty();
-	private final BooleanProperty gameStopped = new SimpleBooleanProperty();
-	private boolean movingDown = true;
-	private boolean movingRight = true;
-	private double movingSpeed = 1.0;
+					startButton, infotext, link, paddle, gameOverText).build();
 
 	private final EventHandler<ActionEvent> pulseEvent = new EventHandler<ActionEvent>() {
 		@Override
@@ -95,13 +124,17 @@ public class HyperBallsMain extends Application {
 	}
 
 	protected void checkCollisions() {
+		if (ball.intersects(paddle.getBoundsInLocal())) {
+			incrementSpeed();
+			movingDown = false;
+		}
 		if (ball.intersects(borderTop.getBoundsInLocal())) {
 			incrementSpeed();
 			movingDown = true;
 		}
 		if (ball.intersects(borderBottom.getBoundsInLocal())) {
-			incrementSpeed();
-			movingDown = false;
+			gameStopped.set(true);
+			gameLost.set(true);
 		}
 		if (ball.intersects(borderLeft.getBoundsInLocal())) {
 			incrementSpeed();
@@ -114,7 +147,7 @@ public class HyperBallsMain extends Application {
 	}
 
 	private void incrementSpeed() {
-		if (movingSpeed <= 5)
+		if (movingSpeed <= 4)
 			movingSpeed += movingSpeed * 0.25;
 	}
 
@@ -123,28 +156,21 @@ public class HyperBallsMain extends Application {
 		movingDown = true;
 		movingRight = true;
 		ballX.setValue(250);
-		ballY.setValue(150);
+		ballY.setValue(350);
+		paddleX.setValue(175);
 		startButton.disableProperty().bind(gameStopped.not());
 		ball.centerXProperty().bind(ballX);
 		ball.centerYProperty().bind(ballY);
+		paddle.xProperty().bind(paddleX);
 		gameStopped.set(true);
+		gameLost.set(false);
+		gameOverText.visibleProperty().bind(gameLost);
 		area.requestFocus();
 	}
 
 	private void initGui(final Stage stage) {
-		Scene scene = SceneBuilder
-				.create()
-				.width(700)
-				.height(500)
-				.fill(LinearGradientBuilder
-						.create()
-						.startX(0.0)
-						.startY(0.0)
-						.endX(0.0)
-						.endY(1.0)
-						.stops(new Stop(0.0, Color.BLACK),
-								new Stop(0.0, Color.GRAY)).build()).root(area)
-				.build();
+		Scene scene = SceneBuilder.create().width(700).height(500)
+				.fill(Color.GRAY).root(area).build();
 		stage.setScene(scene);
 		stage.setTitle("HyperBalls");
 		stage.show();
