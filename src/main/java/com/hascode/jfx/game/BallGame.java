b3aa822ed237fec34374d1f7b531c2cee461b74b
Micha Kops <micha.kops@gmail.com>
2013-06-22 22:13:02 +0200

    Gradle JavaFX plugin added.

--- src/main/java/com/hascode/jfx/game/BallGame.java
+++ src/main/java/com/hascode/jfx/game/BallGame.java
@@ -15,8 +15,7 @@ public class BallGame extends Application {
 	private static final String VIEW_GAME = "/view/GameView.fxml";
 	private static final String STYLESHEET_FILE = "/stylesheet/style.css";
 	public static final Image ICON = new Image(
-			SingleClassNoXmlBallGame.class
-					.getResourceAsStream("/image/head.png"));
+			BallGame.class.getResourceAsStream("/image/head.png"));
 
 	@Override
 	public void start(final Stage stage) throws Exception {
